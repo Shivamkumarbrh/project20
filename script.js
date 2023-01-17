9b6de2d4557ba5b76cb4fd34b5025671ae6b4282
Shivam Kumar <shivamkumar271855@gmail.com>
2023-01-17 23:22:17 +0530

    Marketing / Website Template

---
+++ script.js
@@ -0,0 +1,35 @@
+
+let openIt = ()=>{
+    document.querySelector('.ncation').style.display = 'block'
+}
+let closeIt =  ()=>{
+    document.querySelector('.ncation').style.display = 'none'
+}
+let ddownenter = ()=>{
+    document.querySelector('.dropdown-content').style.display = 'block'
+}
+let ddownleave = ()=>{
+    document.querySelector('.dropdown-content').style.display = 'none'
+}
+let sidebtnopen = ()=>{
+    document.querySelector('.sidenavbar').style.display = 'block'
+}
+let sidebtnclose = ()=>{
+    document.querySelector('.sidenavbar').style.display = 'none'
+}
+let vis1 = ()=>{
+let p1 = document.getElementById('lastSecond')
+p1.style.visibility = 'visible'
+}
+let visOff   = ()=>{
+let p1 = document.getElementById('lastSecond')
+p1.style.visibility = 'hidden'
+}
+let navOpen = ()=>{
+    let p1 = document.querySelector('.navbarMobile1');
+    p1.style.display = 'block'
+}
+let navClose = ()=>{
+    let p1 = document.querySelector('.navbarMobile1');
+    p1.style.display = 'none'
+}
